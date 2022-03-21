b3edad4f5ed6a98fdeb71f60611af066d6abd23d
prebitha.abraham@gmail.com <prebitha.abraham@gmail.com>
2022-03-21 10:01:26 +0000

    update

--- models/customer_rental_data.sql
+++ models/customer_rental_data.sql
@@ -1,4 +1,12 @@
-select customer.*, rental.* 
+select
+
+customer_ID as customerid,
+Gender,
+Registration_Datetime_Local,
+Rental_First_Datetime_Local,
+Age_Years,
+
+rental.* 
 from 
  {{source('rentals', 'customer')}}
  LEFT JOIN  {{source('rentals', 'rental')}}
